14095aa5b19477578a51e9b0de3427fb76e05528
H-Criqui <hec26@humboldt.edu>
2019-11-04 14:49:42 -0800

    Finished adding 2d vector;

--- main.cpp
+++ main.cpp
@@ -11,6 +11,8 @@ using namespace std;
 
 int main()
 {
+
+/*  
 	srand(time(NULL));
 	string temp;
 	string file_name;
@@ -79,11 +81,70 @@ int main()
 		}
 		i++;
 	}
+	*/
+	srand(time(NULL));
+	string temp;
+	string file_name;
+	string out_file_name;
+	int set_effect;
+	cout << "What .ppm file should I open?" << endl;
+	cin >> file_name;
+	cout << endl;
+	cout << "What file should be our destination?" << endl;
+	cin >> out_file_name;
 
+
+	ofstream fout;
+	fout.open(out_file_name);
+	ifstream fin;
+	fin.open(file_name);
+
+	fout.good();
+
+	string file_type;
+	string width;
+	string height;
+	string max_val;
+	getline(fin, file_type);
+	fout << file_type << endl;
+	getline(fin, width, ' ');
+	fout << width << ' ';
+	getline(fin, height);
+	fout << height << endl;
+	getline(fin, max_val);
+	fout << max_val << endl;
+
+	vector<vector<string>> numbers{};
+	string temp;
+	int i = 1;
+	int x = 0;
+	string temporary;
+	while (fin.good())
+	{
+		vector<string> data{};
+		for (i = 1; i <= (stoi(width) * 3); i++)
+		{
+			getline(fin, temp);
+			stringstream ss;
+			ss << temp;
+			while (ss.good())
+			{
+				getline(ss, temporary, ' ');
+				if (temporary != " " && temporary != "")
+				{
+					data.push_back(temporary);
+				}
+			}
+		}
+		numbers.push_back(data);
+	}
+
+
+	
 	bool _continue = true;
 	char user_input = 'y';
 	
-
+	int j = 0;
 	while (_continue)
 	{
 	
@@ -96,44 +157,56 @@ int main()
 		cin >> set_effect;
 
 		i = 0;
+		j = 0;
 		if (set_effect == 1)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if (i % 3 == 0)
+				while (j < (stoi(width) * 3))
 				{
-					numbers[i] = '0';
+					if (j % 3 == 0)
+					{
+						numbers[i][j] = '0';
+					}
+					j++;
 				}
-			
 				i++;
 			}
 		}
 
 		i = 0;
+		j = 0;
 		if (set_effect == 2)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if ((i - 1) % 3 == 0)
+				while (j < stoi(width) * 3)
 				{
-					numbers[i] = '0';
+					if ((j - 1) % 3 == 0)
+					{
+						numbers[i][j] = '0';
+					}
+					j++;
 				}
-			
 
 				i++;
 			}
 		}
 
 		i = 0;
+		j = 0;
 		if (set_effect == 3)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if ((i - 2) % 3 == 0)
+				while (j < stoi(width) * 3)
 				{
-					numbers[i] = '0';
+					if ((j - 2) % 3 == 0)
+					{
+						numbers[i][j] = '0';
+					}
+					j++;
 				}
-
 
 				i++;
 			}
@@ -141,17 +214,21 @@ int main()
 
 
 		i = 0;
+		j = 0;
 		if (set_effect == 4)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if ((i) % 3 == 0)
+				while (j < stoi(width) * 3)
 				{
+					if ((j) % 3 == 0)
+					{
 
-					int temp_num = (255 - stoi(numbers[i]));
-					numbers[i] = to_string(temp_num);
+						int temp_num = (255 - stoi(numbers[i][j]));
+						numbers[i][j] = to_string(temp_num);
+					}
+					j++;
 				}
-		
 				i++;
 			}
 		}
@@ -159,51 +236,63 @@ int main()
 
 
 		i = 0;
+		j = 0;
 		if (set_effect == 5)
 		{
 			while (i < (numbers.size() - 1))
-			{
-				if ((i - 1) % 3 == 0)
+			{	
+				while (j < stoi(width) * 3)
 				{
-					int temp_num = (255 - stoi(numbers[i]));
-					numbers[i] = to_string(temp_num);
+					if ((j - 1) % 3 == 0)
+					{
+						int temp_num = (255 - stoi(numbers[i][j]));
+						numbers[i][j] = to_string(temp_num);
+					}
+					j++;
 				}
-
 				i++;
 			}
 		}
 
 
 		i = 0;
+		j = 0;
 		if (set_effect == 6)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if ((i - 2) % 3 == 0)
+				while (j < stoi(width) * 3)
 				{
-					int temp_num = (255 - stoi(numbers[i]));
-					numbers[i] = to_string(temp_num);
+					if ((j - 2) % 3 == 0)
+					{
+						int temp_num = (255 - stoi(numbers[i][j]));
+						numbers[i][j] = to_string(temp_num);
+					}
+					j++;
 				}
-
 				i += 1;
 			}
 		}
 
 
 		i = 0;
+		j = 0;
 		if (set_effect == 7)
 		{
 			while (i < (numbers.size() - 1))
-			{
-				if (i % 3 == 0)
+			{	
+				while (j < stoi(width) * 3)
 				{
-					int temp_num = ((stoi(numbers[i]) + stoi(numbers[i + 1]) + stoi(numbers[i + 2])) / 3);
+					if (j % 3 == 0)
+					{
+						int temp_num = ((stoi(numbers[i][j]) + stoi(numbers[i][j + 1]) + stoi(numbers[i][j + 2])) / 3);
 
-					numbers[i] = temp_num;
-					numbers[i + 1] = temp_num;
-					numbers[i + 2] = temp_num;
+						numbers[i][j] = temp_num;
+						numbers[i][j + 1] = temp_num;
+						numbers[i][j + 2] = temp_num;
+					}
+					j++;
 				}
-
 				i++;
 			}
 		}
@@ -214,65 +303,74 @@ int main()
 		int num_3;
 
 		i = 0;
+		j = 0;
 		if (set_effect == 8)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if (i % 3 == 0)
+				while(j < stoi(width)*3)
 				{
-					rand_num = ((rand() % 14) - 7);
-					num_1 = stoi(numbers[i]) + rand_num;
-					num_2 = stoi(numbers[i + 1]) + rand_num;
-					num_3 = stoi(numbers[i + 2]) + rand_num;
+					if (j % 3 == 0)
+					{
+						rand_num = ((rand() % 14) - 7);
+						num_1 = stoi(numbers[i][j]) + rand_num;
+						num_2 = stoi(numbers[i][j + 1]) + rand_num;
+						num_3 = stoi(numbers[i][j + 2]) + rand_num;
 
-					if (num_1 < 0)
-					{
-						num_1 = 0;
-					}
-					else if (num_1 > 255)
-					{
-						num_1 = 255;
-					}
+						if (num_1 < 0)
+						{
+							num_1 = 0;
+						}
+						else if (num_1 > 255)
+						{
+							num_1 = 255;
+						}
 
-					if (num_2 < 0)
-					{
-						num_2 = 0;
-					}
-					else if (num_2 > 255)
-					{
-						num_2 = 255;
-					}
+						if (num_2 < 0)
+						{
+							num_2 = 0;
+						}
+						else if (num_2 > 255)
+						{
+							num_2 = 255;
+						}
 
-					if (num_3 < 0)
-					{
-						num_3 = 0;
-					}
-					else if (num_3 > 255)
-					{
-						num_3 = 255;
-					}
-					
-					numbers[i] = to_string(num_1);
-					numbers[i + 1] = to_string(num_2);
-					numbers[i + 2] = to_string(num_3);
-				}
+						if (num_3 < 0)
+						{
+							num_3 = 0;
+						}
+						else if (num_3 > 255)
+						{
+							num_3 = 255;
+						}
 
+						numbers[i][j] = to_string(num_1);
+						numbers[i][j + 1] = to_string(num_2);
+						numbers[i][j + 2] = to_string(num_3);
+					}
+				j++;
+			}
 				i++;
 			}
 		}
 
 		i = 0;
+		j = 0;
 		if (set_effect == 9)
 		{
 			while (i < (numbers.size() - 1))
 			{
-				if (stoi(numbers[i]) > 127)
+				while (j < stoi(width) * 3)
 				{
-					numbers[i] = "255";
-				}
-				else
-				{
-					numbers[i] = "0";
+					if (stoi(numbers[i][j]) > 127)
+					{
+						numbers[i][j] = "255";
+					}
+					else
+					{
+						numbers[i][j] = "0";
+					}
+					j++;
 				}
 				i++;
 			}
@@ -290,11 +388,15 @@ int main()
 
 	for (i = 0; i < numbers.size(); i++)
 	{
-		fout << numbers[i] << " ";
-
-		if (i % 3 == 0)
+		while (j < stoi(width) * 3)
 		{
-			fout << endl;
+			fout << numbers[i][j] << " ";
+
+			if (i % 3 == 0)
+			{
+				fout << endl;
+			}
+			j++;
 		}
 	}
 	
